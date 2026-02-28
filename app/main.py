b98bc6ead1d296425b4157d765976f46ed8514bc
Claris-FF15 <clarisse.binard@outlook.fr>
2026-02-28 12:52:51 +0100

    feat: modify to create table in pgadmin
    refs: ticket-21

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from routers.user_router import router as user_router
+from db.database import Base, engine  # ton engine SQLAlchemy
+from models.user import User
+from models.badge import Badge
 
 app = FastAPI()
+
+# Crée toutes les tables au démarrage si elles n'existent pas
+Base.metadata.create_all(bind=engine)
+
 app.include_router(user_router)
